106ab3250329768ce397e3fbdba2f8cffe6ba3ac
Carlos Guzman Casanova <carlos.guzman@milano-melody.net>
2022-02-17 19:08:20 -0600

    first commit

---
+++ general.js
@@ -0,0 +1,11 @@
+let res = 1;
+
+function showAlert() {
+    console.log(res);
+    alert("Alert from JS file");
+}
+
+
+function showErrores() {
+    console.log("log Errors");
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+var init = true;
+
+function fnConsultar() {
+    console.log(init);
+}
+
+
+function fnError() {
+    console.log("Error");
+}
